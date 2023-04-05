5e7320d078a891225d270172b3d93131e6931f75
Liam1017 <elijahturany1415@gmail.com>
2023-04-05 12:40:43 -0500

    2023/04/05 12:40

--- Pro7/ObscuredStructure.h
+++ Pro7/ObscuredStructure.h
@@ -1,39 +1,82 @@
 #pragma once
 #include <vector>
+using namespace std;
 
 class ObscuredStructure {
 public:
+    //Virtual method declarations
     virtual void add(int data) = 0;
     virtual void remove() = 0;
     virtual int peek() = 0;
     virtual int size() = 0;
 };
 
-//Stack
+//Stack class wrapped around a vector
 class Child1 : public ObscuredStructure {
 private:
-    //vector<int> internalVec;
-    int internal = 0;
+    vector<int> internalVec;
 public:
     Child1() {}
 
+    //Pushes data to the back of the vector
     void add(int data) {
-        //internalVec.push_back(data);
-        internal++;
+        internalVec.push_back(data);
     }
 
+    //Removes data from the back of the vector
     void remove() {
-        //internalVec.pop_back();
-        internal--;
+        if (internalVec.size() > 0) {
+            internalVec.pop_back();
+        }
     }
 
+    //Returns the last item of the vector if the vector isn't empty
     int peek() {
-        //return internalVec[internalVec.size() -1];
-        return internal;
+        if (internalVec.size() > 0) {
+            return internalVec[internalVec.size() -1];
+        }
+        else {
+            throw "Structure is empty";
+        }
     }
 
+    //Returns size
     int size() {
-        //return internalVec.size();
-        return internal;
+        return internalVec.size();
+    }
+};
+
+//Queue class wrapped around a vector
+class Child2 : public ObscuredStructure {
+private:
+    vector<int> internalVec;
+public:
+    Child2() {}
+
+    //Pushes data to the back of the vector
+    void add(int data) {
+        internalVec.push_back(data);
+    }
+
+    //Removes data from the front of the vector
+    void remove() {
+        if (internalVec.size() > 0) {
+            internalVec.erase(internalVec.begin());
+        }
+    }
+
+    //Returns the first item of the vector if the vector isn't empty
+    int peek() {
+        if (internalVec.size() > 0) {
+            return internalVec[0];
+        }
+        else {
+            throw "Structure is empty";
+        }
+    }
+
+    //Returns size
+    int size() {
+        return internalVec.size();
     }
 };
--- Pro7/Pro7.cpp
+++ Pro7/Pro7.cpp
@@ -8,5 +8,36 @@ int main(){
     ch1.add(2);
     ch1.add(3);
     cout << ch1.peek();
-    
+    ch1.remove();
+    cout << ch1.peek();
+    ch1.remove();
+    cout << ch1.peek() << endl;
+    ch1.remove();
+    ch1.remove();
+
+    Child2 ch2;
+    ch2.add(4);
+    ch2.add(5);
+    ch2.add(6);
+    cout << ch2.peek();
+    ch2.remove();
+    cout << ch2.peek();
+    ch2.remove();
+    cout << ch2.peek() << endl;
+    ch2.remove();
+    ch2.remove();
+
+    try {
+        ch1.peek();
+    }
+    catch (...) {
+        cout << "Ch1 is empty" << endl;
+    }
+
+    try {
+        ch2.peek();
+    }
+    catch (...) {
+        cout << "Ch2 is empty" << endl;
+    }
 }
